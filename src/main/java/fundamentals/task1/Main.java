bfcb6b61dedb038cce34a776e9f9c7da26b21d41
Daniil Novikau <d.nagib@yandex.ru>
2021-01-17 21:36:51 +0300

    Task1 completed

---
+++ src/main/java/fundamentals/task1/Main.java
@@ -0,0 +1,13 @@
+package fundamentals.task1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, " + scanner.nextLine());
+
+    }
+
+}
